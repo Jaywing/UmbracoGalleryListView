b59209743076c89e436c6961566e9fad0adfadca
Gareth Wright <garpunkal@gmail.com>
2018-06-19 10:02:58 +0100

    readme and files

---
+++ App_Plugins/GalleryListView/galleryListView.controller.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    "use strict";
+
+    function GalleryListViewController($scope, listViewHelper, $location, mediaResource, mediaHelper) {
+
+        var vm = this;
+
+        vm.selectItem = selectItem;
+        vm.clickItem = clickItem;
+
+        function activate() {
+            angular.forEach($scope.items, function (item) {
+                getBackgroundImage(item);
+            });            
+        }
+
+        function getBackgroundImage(item) {   
+            mediaResource.getById(item.image)
+                .then(function (media) {
+                    item.thumbnail = mediaHelper.resolveFile(media, true);
+                });
+        }
+
+        function selectItem(selectedItem, $event, index) {
+            listViewHelper.selectHandler(selectedItem, index, $scope.items, $scope.selection, $event);
+            $event.stopPropagation();
+        }
+
+        function clickItem(item) {
+            $location.path($scope.entityType + '/' + $scope.entityType + '/edit/' + item.id);
+        }
+
+        activate();
+    }
+    angular.module("umbraco").controller("Jaywing.GalleryListView.GalleryListViewController", GalleryListViewController);
+})();
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # GalleryListView
+
+A simple app_plugin for Umbraco that allows you to view a ListView in the style of the Media Library. 
+
+[!Screenshot](screenshot.png)
+
+[!DataType setup](datatype.png)
+
+Fork of the following blog post: https://24days.in/umbraco-cms/2016/custom-list-view-layouts/
